865755bb81b619708a0e1c8e9dc1288649836d64
darshaksachapara <offcan1@gmail.com>
2026-07-26 19:31:46 +0530

    chore: configure custom domain www.kimirefer.space

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/moonshoot-kimi-referal/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
